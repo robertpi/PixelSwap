971555d6f1c9ac7238dbfa8c3785753a6c1b4df2
Robert Pickering <robert@sqreen.io>
2021-01-19 12:58:59 +0100

    C# starter

---
+++ csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var img = (Bitmap)Image.FromFile("../../../../images/lenna.png");
+            for (int x = 0; x < img.Width - 1; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    var c = img.GetPixel(x, y);
+                    img.SetPixel(x, y, Color.FromArgb(0, c.G, c.B));
+                }
+
+            }
+
+            img.Save("../../../../images/lenna-no-red.png");
+        }
+    }
+}
